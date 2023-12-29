7aebe6f09aa18faf8dc7d9d190f6d764513f7ee8
Arijit Bhowmik <122301763+ARIJIT0483@users.noreply.github.com>
2023-12-29 16:08:29 +0530

    cpmleted

---
+++ script.js
@@ -0,0 +1,57 @@
+const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies"
+const dropdown = document.querySelectorAll('.dropdown select')
+const btn = document.querySelector('form button')
+const fromCurr = document.querySelector('.from select')
+const toCurr = document.querySelector('.to select')
+const msg = document.querySelector('.msg')
+
+window.addEventListener("load", () => {
+    updateExchangeValue()
+})
+
+for(let select of dropdown){
+    for (currCode in countryList)
+{
+    let newOption = document.createElement('option')
+    newOption.innerText = currCode
+    newOption.value = currCode
+    if(select.name == 'from' && currCode == 'USD'){
+        newOption.selected='selected'
+    }else if(select.name == 'to' && currCode == 'INR'){
+        newOption.selected='selected'
+    }
+    select.appendChild(newOption)
+}
+select.addEventListener('change', (event) => {
+    updateFlag(event.target)
+})
+}
+
+const updateFlag = (element) => {
+    let currCode = element.value
+    let countryCode = countryList[currCode]
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`
+    let imgFlag = element.parentElement.querySelector('img')
+    imgFlag.src = newSrc
+}
+
+btn.addEventListener("click", (event) => {
+    event.preventDefault()
+    updateExchangeValue()
+})
+const updateExchangeValue = async () => {
+    let amount = document.querySelector('.amount input')
+    let amtval = amount.value
+    if(amtval == "" || amtval < 1){
+        amtval = 1
+        amount.value = "1"
+    }
+
+    console.log(fromCurr)
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`
+    let response = await fetch(URL)
+    let data = await response.json()
+    let rate = data[toCurr.value.toLowerCase()]
+    let finalAmount = amtval*rate
+    msg.innerText = `${amtval} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`
+}
